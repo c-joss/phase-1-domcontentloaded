ece637082e025c0ada63a88ac3d9b4f8e80f5500
Courtney Macgregor <cjoss90@hotmail.com>
2025-01-05 11:39:25 +1000

    Done assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+
+    const textElement = document.querySelector("#text");
+
+    if (textElement) {
+        textElement.textContent= "This is really cool!";
+    }
+});
+
+console.log(document.id("text").target);
